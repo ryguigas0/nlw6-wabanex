fab6797c5f573b7bc1d912ba901ebab5259792f0
ryguigas0 <guigacasa@protonmail.ch>
2021-06-27 11:47:41 -0300

    Added loading user most recent active training functionallity

--- lib/wabanex/users/get.ex
+++ lib/wabanex/users/get.ex
@@ -1,9 +1,9 @@
 defmodule Wabanex.Users.Get do
+  import Ecto.Query
 
-  alias Wabanex.{Repo, User}
+  alias Wabanex.{Training, Repo, User}
 
   def call(id) do
-
     id
     |> Ecto.UUID.cast()
     |> handle_response
@@ -13,7 +13,18 @@ defmodule Wabanex.Users.Get do
     # Gets data by id and parses into a struct
     case Repo.get(User, uuid) do
       nil -> {:error, "User not found"}
-      user -> {:ok, user}
+      user -> {:ok, load_training(user)}
     end
+  end
+
+  defp load_training(user) do
+    today = Date.utc_today()
+
+    # Check if the training is valid -> Have started or not ended before today
+    query =
+      from t in Training,
+        where: ^today >= t.start_date and ^today <= t.end_date
+
+    Repo.preload(user, trainings: {first(query, :inserted_at), :exercises})
   end
 end
